1f75b84dcbb713f38251cb5689a50c21d3cc0886
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-06-27 12:28:19 -0700

    Create Condensr.php

---
+++ plugins/Condensr.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+  This plugin provides access to the Condensr.io API for condensing longform text.
+*/
+
+function Condensr($LongformText,$NumberOfSentences=1){
+   $URL='https://api.condensr.io/v1';
+  
+  $Arguments=array(
+    'LongformText'      => $LongformText,
+    'NumberOfSentences' => $NumberOfSentences
+  );
+  
+  //Set up cURL  
+  $cURL = curl_init();
+  curl_setopt($cURL,CURLOPT_URL, $URL);
+  curl_setopt($cURL,CURLOPT_POST, count($Arguments));
+  $URLArguments = http_build_query($Arguments);
+  curl_setopt($cURL,CURLOPT_POSTFIELDS, $URLArguments);
+  curl_setopt($cURL,CURLOPT_RETURNTRANSFER, true);
+  
+  //Run cURL and close it
+  $Data = curl_exec($cURL);
+  if(curl_exec($cURL) === false){
+    echo 'Curl error: ' . curl_error($cURL);
+  }
+  curl_close($cURL);
+  
+  $Data=json_decode($Data,true);
+  
+  return $Data;
+}
